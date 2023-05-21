9d44d2e39757071c99be7d435d9b07c98577ca9f
meiliou <83768277+meiliou@users.noreply.github.com>
2023-05-21 13:35:22 +0400

    create, view,  update and delete posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// We also need to make the reverse association
+// Post can belong to one user, but not many users
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Post };
