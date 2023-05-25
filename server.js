d2f5e362cf2e470a561bab764dca4402b187650d
meiliou <83768277+meiliou@users.noreply.github.com>
2023-05-25 09:16:20 +0400

    homepage view added

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set up handlebars.js engine with custom helpers
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// serve up static assets (useful for front-end and JS files)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
