e0441534df5b6ba62c6429f7f4d3ac72806a9115
meiliou <83768277+meiliou@users.noreply.github.com>
2023-05-27 21:47:14 +0400

    logout functionality added

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// create session
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+const hbs = exphbs.create({});
 
 // set up handlebars.js engine with custom helpers
 app.engine('handlebars', hbs.engine);
@@ -17,9 +32,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // serve up static assets (useful for front-end and JS files)
 app.use(express.static(path.join(__dirname, 'public')));
+// session middleware
+app.use(session(sess));
 
 // turn on routes
-app.use(routes);
+app.use(routes); //same as app.use(require('./controllers'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
